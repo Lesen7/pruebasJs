eef5ee1536d98941689019a6acde83a05adf1536
Enzo Lespart <enzolespart@gmail.com>
2017-12-19 00:47:28 +0000

    calculadora creada

--- index.js
+++ index.js
@@ -1,7 +1,50 @@
 var numero1 = prompt('Dame un número');
+var operador = prompt('Dame el signo de operación');
 var numero2 = prompt('Dame otro número');
+var tipoDivision;
 var resultado;
+var cocienteDivision;
 
-resultado = numero1 + numero2;
+function decimalAEntero (valor) {
+    return valor | 0;
+}
 
-alert('el resultado es ' + resultado);
+function determinarOperador () {
+  switch (operador) {
+    case "+":
+      resultado = numero1 + numero2;
+      break;
+
+      case "-":
+        resultado = numero1 - numero2;
+        break;
+
+        case "*":
+        resultado = numero1 * numero2;
+        break;
+
+        case "/":
+        tipoDivision = prompt('Especifica el tipo de division: entera o decimal');
+        resultado = numero1 / numero2;
+        cocienteDivision = numero1 % numero2;
+        break;
+
+  default :
+    resultado = "inválido";
+  }
+}
+
+function mostrarResultado (validez) {
+  if (validez === "inválido") {
+    alert('Has introducido datos inválidos');
+  } else if (operador != "/") {
+      alert('El resultado es ' + resultado);
+  }  else if (tipoDivision === "entera"){
+      alert('El resultado es ' + Math.round(resultado) + ' y el resto de la división es ' + cocienteDivision);
+  } else {
+      alert('El resultado es ' + resultado);
+  }
+}
+
+determinarOperador();
+mostrarResultado(resultado);
